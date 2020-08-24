4de4c05b9ae912bc29158a5d59e6003cf6ef40b9
tegarimansyah <egar.imansyah@gmail.com>
2020-08-24 15:11:44 +0700

    xpander

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,9 @@
 echo "Start"
 
 # Get data from github action
-SRC_DIR="$1"
-DEST_GITHUB_USERNAME="$2"
-USER_EMAIL="$3"
-DEST_BRANCH="$4"
+DEST_GITHUB_USERNAME="$1"
+USER_EMAIL="$2"
+DEST_BRANCH="$3"
 
 SOURCE_TABLE="list_system.csv" # get list of system
 SYSTEM_NAME=$(git log -1 --pretty=%B) # get which system from git commit
@@ -16,11 +15,12 @@ OLDIFS=$IFS
 IFS=',' # separate line with comma
 [ ! -f $SOURCE_TABLE ] && { echo "$SOURCE_TABLE file not found"; exit 99; } # check if file is available
 
-while read Repo URL # will read $SOURCE_TABLE 
+while read REPO URL INITIATIVE # will read $SOURCE_TABLE 
 do
-    if [ $SYSTEM_NAME == $Repo ];
+    if [ $SYSTEM_NAME == $REPO ];
     then
       DEST_GITHUB_REPO=$URL
+      INITIATIVE_NAME=$INITIATIVE
       break
     fi 
 done < $SOURCE_TABLE
@@ -28,11 +28,11 @@ IFS=$OLDIFS
 
 if [ -z "$DEST_GITHUB_REPO" ]; # check if commit msg is valid
 then
-  echo "Should exit"
+  echo "\"$SYSTEM_NAME\" not found in table"
   exit 1
 fi 
 
-
+SRC_DIR="docs/$INITIATIVE_NAME/$SYSTEM_NAME"
 CLONE_DIR=$(mktemp -d)
 
 # Setup git
@@ -40,11 +40,11 @@ git config --global user.email "$USER_EMAIL"
 git config --global user.name "$DEST_GITHUB_USERNAME"
 git clone --single-branch -b $DEST_BRANCH "https://$API_TOKEN_GITHUB@$DEST_GITHUB_REPO.git" "$CLONE_DIR"
 
-echo 'Copying from '"$SRC_DIR"'/*' "to $CLONE_DIR/$SYSTEM_NAME"
-mkdir -p "$CLONE_DIR/$SYSTEM_NAME"
-cp -R "$SRC_DIR"/* "$CLONE_DIR/$SYSTEM_NAME" || (rm -Rf "$CLONE_DIR" && exit 1)
+echo 'Copying from '"$SRC_DIR"'/*' "to $CLONE_DIR/docs"
+mkdir -p "$CLONE_DIR/docs"
+cp -R "$SRC_DIR"/* "$CLONE_DIR/docs/" || (rm -Rf "$CLONE_DIR" && exit 1)
 
 cd "$CLONE_DIR"
 git add .
 git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
-git push origin $DEST_BRANCH
+# git push origin $DEST_BRANCH
